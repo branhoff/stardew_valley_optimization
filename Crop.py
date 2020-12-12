b4dccfe88776659fcfff64ef81f9b791096217ed
branhoff <branhoff2@gmail.com>
2020-12-12 07:01:33 -0800

    refactored

--- Crop.py
+++ Crop.py
@@ -27,6 +27,9 @@ class Crop:
         return self._intial_grow
             
     def shop_closed(self, start=0):
+        """
+
+        """
         end = Crop._season_duration
         
         return [x + 1 for x in range(start-1,end) if (x + 1) % 7 == 3]
@@ -48,7 +51,7 @@ class NonRenewCrop(Crop):
         
         return [x+start-1 if isharvest(x) and not isclosed(x+1) else x+start for x in range(start,end+1) if isharvest(x)]        
 
-    def compound(self, account=500):
+    def compound_return(self, account=500):
         for _x in self.pay_days():
             remainder = account % self._cost
             revenue = ((account - (remainder))/self._cost)*self._sell_price
@@ -63,13 +66,15 @@ class NonRenewCrop(Crop):
 
 class RenewCrop(Crop):
         
-    def __init__(self, crop, cost, sell_price, growth_duration, intial_grow):
-        super().__init__(crop, cost, sell_price, growth_duration, intial_grow)
+    def __init__(self, _crop, _cost, _sell_price, _growth_duration, _intial_grow):
+        super().__init__(_crop, _cost, _sell_price, _growth_duration, _intial_grow)
 
         self.leftover_days = Crop._season_duration % self._growth_duration
         self.harvest_count = int((Crop._season_duration - self.leftover_days) / self._growth_duration + 1)
         
-    def compound(self, account=500):
-        #for x in 
+    def compound_return(self, account=500):
+        """
+
+        """
         
         pass
