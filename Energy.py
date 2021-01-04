5f5c7bf495d44d20617a1f4d2495b4b001b342d4
branhoff <branhoff2@gmail.com>
2021-01-03 21:20:24 -0800

    Added Energy class model how much energy is available to work crops

---
+++ Energy.py
@@ -0,0 +1,62 @@
+class Energy:
+
+    _intial_energy = 270
+    _current_energy = _intial_energy
+    _max_perm_energy = 508 # max energy after stadrop upgrades
+    _max_temp_energy = 588 # temp energy can currently be 50
+    _stardrop_count = 0
+
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def stardrop_additons(self, stardrop_count):
+        """
+
+        """
+        pass
+
+    def energy_usage(self):
+        """
+
+        """
+        pass
+
+    def axe_usage(self, skill_level=0, power_ups=0):
+        """
+
+        """
+        pass
+
+    def hoe_usage(self, skill_level=0, power_ups=0):
+        """
+
+        """
+        pass    
+
+    def pickaxe_usage(self, skill_level=0, power_ups=0):
+        """
+
+        """
+        pass    
+
+    def watercan_usage(self, skill_level=0, power_ups=0):
+        """
+
+        """
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
