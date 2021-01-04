93bd7b7181562efa95d2b191e9e5f901a3126e89
branhoff <branhoff2@gmail.com>
2021-01-03 21:21:19 -0800

    updated Crop and children classes

--- Crop.py
+++ Crop.py
@@ -4,12 +4,12 @@ class Crop:
     
     _season_duration = 28
     
-    def __init__(self, crop, cost, sell_price, growth_duration, intial_grow=None):
+    def __init__(self, crop, cost, sell_price, first_growth_rate, second_growth_rate=None):
         self._crop = crop
         self._cost = cost
         self._sell_price = sell_price
-        self._growth_duration = growth_duration
-        self._intial_grow = intial_grow
+        self._first_growth_rate = first_growth_rate
+        self._second_growth_rate = second_growth_rate
     
     def get_crop(self):
         return self._crop
@@ -20,33 +20,37 @@ class Crop:
     def get_sell_price(self):
         return self._sell_price
     
-    def get_growth_duration(self):
-        return self._growth_duration
+    def get_first_growth_rate(self):
+        return self._first_growth_rate
     
-    def get_intial_grow(self):
-        return self._intial_grow
+    def get_second_growth_rate(self):
+        return self._second_growth_rate
             
-    def shop_closed(self, start=0):
+    def shop_closed(self, start=1):
         """
 
         """
         end = Crop._season_duration
         
-        return [x + 1 for x in range(start-1,end) if (x + 1) % 7 == 3]
+        return [x for x in range(start,end+1) if x % 7 == 3]
     
 
 class NonRenewCrop(Crop):
         
-    def __init__(self, _crop, _cost, _sell_price, _growth_duration):
-        super().__init__(_crop, _cost, _sell_price, _growth_duration)
+    def __init__(self, _crop, _cost, _sell_price, _first_growth_rate):
+        super().__init__(_crop, _cost, _sell_price, _first_growth_rate)
 
-        self._leftover_days = Crop._season_duration % self._growth_duration
-        self._harvest_count = int((Crop._season_duration - self._leftover_days) / self._growth_duration + 1)
+        self._leftover_days = Crop._season_duration % self._first_growth_rate
+        self._harvest_count = int((Crop._season_duration - self._leftover_days) / self._first_growth_rate + 1)
         
     def pay_days(self, start=1):
+        """
+        returns a list of days that you can harvest crops and sell it back to the shop
+        i.e. crop is mature and store is not closed
+        """
         end = Crop._season_duration
         
-        isharvest = lambda x: x % (self._growth_duration + 1) == 0
+        isharvest = lambda x: x % (self._first_growth_rate + 1) == 0
         isclosed = lambda x: x % 7 == 3
         
         return [x+start-1 if isharvest(x) and not isclosed(x+1) else x+start for x in range(start,end+1) if isharvest(x)]        
@@ -64,17 +68,17 @@ class NonRenewCrop(Crop):
         #print(self.harvest_count)
         pass
 
+
 class RenewCrop(Crop):
         
-    def __init__(self, _crop, _cost, _sell_price, _growth_duration, _intial_grow):
-        super().__init__(_crop, _cost, _sell_price, _growth_duration, _intial_grow)
+    def __init__(self, _crop, _cost, _sell_price, _first_growth_rate, _second_growth_rate):
+        super().__init__(_crop, _cost, _sell_price, _first_growth_rate, _second_growth_rate)
 
-        self.leftover_days = Crop._season_duration % self._growth_duration
-        self.harvest_count = int((Crop._season_duration - self.leftover_days) / self._growth_duration + 1)
+        self.leftover_days = Crop._season_duration % self._first_growth_rate
+        self.harvest_count = int((Crop._season_duration - self.leftover_days) / self._first_growth_rate + 1)
         
     def compound_return(self, account=500):
         """
 
         """
-        
         pass
